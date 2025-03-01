0b3d9bbdd30c8228a825a97729141a080957de70
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-02 03:06:53 +0530

    UC3

--- src/main/java/com/example/helloworld/controller/HelloRestController.java
+++ src/main/java/com/example/helloworld/controller/HelloRestController.java
@@ -1,13 +1,12 @@
 package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloWorldGet {
-    @GetMapping("/helllo")
-    public String sayHello(){
+public class HelloRestController {
+    @RequestMapping("/")
+    public String index(){
         return "Hello from BridgeLabz";
     }
 }
